9326c5430b1cbb09b1814861c83b19f8125ce263
yachuh <yachuh@gmail.com>
2022-06-14 15:57:07 +0800

    Update to React 18.1.0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// React 17 (old way)
+// ReactDOM.render(<App />, document.getElementById('root'));
 
+// React 18
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+//root.render(<App tab="home" />);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
