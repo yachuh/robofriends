969b2925486e20b160e7def2b005fce6f08130bf
yachuh <yachuh@gmail.com>
2022-06-15 11:39:26 +0800

    Create states using useState hook

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
+function App () {
+    // constructor() {
+    //     super()
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: ''
+    //     }
+    // }
 
-    componentDidMount() {
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then(response => response.json())
-            .then(users => this.setState( {robots: users}));
-    }
+    // Declare states
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
+    // componentDidMount() {
+    //     fetch("https://jsonplaceholder.typicode.com/users")
+    //         .then(response => response.json())
+    //         .then(users => this.setState( {robots: users}));
+    // }
 
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter( robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
-        })
-
-        return !robots.length ?
-            <h1 className="tc">Loading</h1> :
-            (
-                <div className="tc">
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filteredRobots}/>   
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            );
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
     }
+    
+    // const { robots, searchfield } = this.state; --> already have access to the state, no longer need to access it through this.state
+    const filteredRobots = robots.filter( robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+
+    // ternary
+    return !robots.length ?
+        <h1 className="tc">Loading</h1> :
+        (
+            <div className="tc">
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundary>
+                        <CardList robots={filteredRobots}/>   
+                    </ErrorBoundary>
+                </Scroll>
+            </div>
+        );
 }
 
 
